9f4b976706c8a58121ad9ee0324b7ecd777f8b7f
NumanArshad <37812712+NumanArshad@users.noreply.github.com>
2018-04-19 01:15:08 +0500

    i created login complete register without  clas

---
+++ Myserver/Myserver/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Myserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdRegister(object sender, EventArgs e)
+        {
+            localhost.Service1 s = new localhost.Service1();
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Incomplete");
+            }
+            else
+            {
+                s.registerUser(textBox1.Text, textBox2.Text);
+                MessageBox.Show("User has been Regisrered Successfully");
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void lblLoginForm(object sender, EventArgs e)
+        {
+            LoginForm f = new LoginForm();
+            f.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Myserver/Myserver/LoginForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Myserver
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdLogin(object sender, EventArgs e)
+        {
+            bool isvalid;
+            bool isvalidpassed;
+            localhost.Service1 s = new localhost.Service1();
+
+            s.isValidUser(textBox1.Text, textBox2.Text, out isvalid, out isvalidpassed);
+            if (isvalid)
+            {
+                MessageBox.Show("Valid User");
+            }
+            else
+            {
+                MessageBox.Show("Invalid User");
+            }
+
+            
+        }
+
+        private void lblRegistration(object sender, EventArgs e)
+        {
+
+            Form1 f1 = new Form1();
+            f1.Show();
+            this.Hide();
+        }
+    }
+}
